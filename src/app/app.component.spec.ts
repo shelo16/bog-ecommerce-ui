cea3d35b38644d9b83c78f7aec60e338a1d12714
Tornike Shelia <tornikeshelia1996@gmail.com>
2020-11-03 00:25:45 +0400

    copy of existing jwt-test-project to new project for BOG

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'jwt-example-ui'`, () => {
+  it(`should have as title 'bog-ecommerce-ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('jwt-example-ui');
+    expect(app.title).toEqual('bog-ecommerce-ui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('jwt-example-ui app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('bog-ecommerce-ui app is running!');
   });
 });
